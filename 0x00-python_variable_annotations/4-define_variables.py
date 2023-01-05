9b173641e6d4cb0cd69e02db366a71341440b3ff
nomso <chinomsokoye@gmail.com>
2023-01-05 18:33:01 +0000

    Define and annotate variables with specified values

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Define variables
+Define and annotate variables with specified values
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
