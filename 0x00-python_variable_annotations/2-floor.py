f2740652af44f5bbb8ba3531d1db66e414ff42a2
nomso <chinomsokoye@gmail.com>
2023-01-05 18:05:32 +0000

    Write type-annotated function floor, takes argument float n, returns floor of the float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - floor
+Write type-annotated function floor with float argument
+Returns floor of the float
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Type-annotated function
+    Floor
+    """
+    return int(n)
