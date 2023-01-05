254dca67ae579fc8224dddbcb43029896203a047
nomso <chinomsokoye@gmail.com>
2023-01-05 18:22:17 +0000

    Write type-annotated function to_str, returns string repr of a float argument

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - to string
+Write typed-annotation function to_str that takes a float argument
+Returns string repr of the float
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Type-annotated function
+    to_str
+    """
+    return str(n)
