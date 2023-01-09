ac3cc4addaf74dfec1ed46d5e9a748137ccae9cf
nomso <chinomsokoye@gmail.com>
2023-01-09 12:20:06 +0000

    Create function measure_time, integer arguments n & max_delay, measures & returns wait_n(n, max_delay), total_time / n as float

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -19,5 +19,5 @@ def measure_time(n: int, max_delay: int) -> float:
     start = time.perf_counter()
     asyncio.run(wait_n(n, max_delay))
     stop = time.perf_counter()
-    total_time = start - stop
+    total_time = stop - start
     return total_time / n
