b0fe83636b2a7f2a1bc5b7dabfeac0beeed92408
nomso <chinomsokoye@gmail.com>
2023-01-09 12:41:14 +0000

    Write function task_wait_random, takes int argument max_delay, returns a asyncio.Task

---
+++ 0x01-python_async_function/3-main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import asyncio
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def test(max_delay: int) -> float:
+    task = task_wait_random(max_delay)
+    await task
+    print(task.__class__)
---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Tasks
+Write function task_wait_random, takes integer argument max_delay 
+Returns a asyncio.Task
+"""
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> async.Task:
+    """
+    Function task_wait_random
+    takes int argument max_delay
+    Returns a async.Task
+    """
+    stop = asyncio.create_task(wait_random(max_delay))
+    return stop
